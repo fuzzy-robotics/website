907c72e59caaa77f3c7b6d21c0e4fbdfd37c9afb
Pratyush-exe <pratyush21225@gmail.com>
2025-05-21 03:52:56 +0530

    added simple logo UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Landing from './pages/Landing'
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AnimatedCursor 
+          trailingSpeed={8}
+          innerSize={8}
+          outerSize={50}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0} 
+          outerStyle={{
+            border: '2px solid rgba(97, 122, 246, 0.26)'
+          }}
+          innerStyle={{
+            backgroundColor: '#474444'
+          }}/>
+      <Landing />
     </>
   )
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../components/Logo';
+
+function Landing() {
+    return (
+        <div className='flex flex-col pt-[100px] pl-[50px]'>
+            <Logo scale={2} />
+            <div className='font-[900] text-[#474444] mt-[50px] pl-[10px]' style={{fontFamily: 'rubik', fontSize: '30px'}}>
+                COMING SOON ...
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
