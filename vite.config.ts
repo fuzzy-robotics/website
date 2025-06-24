902f4dc53498f9e85b8a335b9c9cc847e64903a5
G-Paul <gunjanpaul99.9@gmail.com>
2025-06-24 21:02:18 +0530

    Testing new github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
